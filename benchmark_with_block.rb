b3429847b02a18b78318dfb84c8abae53a5c9c19
laraibsshaikh10 <laraibsshaikh10@gmail.com>
2024-06-23 01:40:50 -0400

    Add benchmark code

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,15 @@
 def benchmark
   # Your benchmarking code goes here.
+  #capture start time
+  start_time = Time.now
+  # yield the benchmark block
+  yield
+  #capture end tiem
+  end_time = Time.now
+  #calculate the running time
+  running_time = end_time - start_time
+  #return 
+  running_time
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
